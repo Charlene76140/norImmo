c39c4c6380887a39d5eea21850650e2c8c662316
Charlene St-Julien <c.saintjulien76140@gmail.com>
2021-04-01 18:45:33 +0200

    add contains on faq

--- js/main_Cha.js
+++ js/main_Cha.js
@@ -1,13 +1,19 @@
 let questions = document.getElementsByClassName("questions");
 
+
 for (i = 0; i < questions.length; i++) {
     questions[i].onclick = function(){
       let answerHidden = this.nextElementSibling;
       if (answerHidden.style.display === "block") {
         answerHidden.style.display = "none";
-      } else {
-        answerHidden.style.display = "block";
       }
+
+       else {
+        answerHidden.style.display = "block";
+       }
     };
 }
+
+
+
 
